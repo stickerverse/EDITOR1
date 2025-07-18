3235d6aa371f4266a986665d48c04a47d1a320f9
Stephen Kirk <kirk11792@gmail.com>
2025-07-18 12:21:01 +0000

    add the nav menu to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
-
 "use client";
+import { NavMenu } from "@/components/nav-menu";
 import { CosmicParallaxBg } from "@/components/ui/parallax-cosmic-background";
 
 export default function HomePage() {
   return (
-    <main>
-      <CosmicParallaxBg 
-        head="Stickerific" 
-        text="Your Vision, Your Sticker, Instantly" 
-        loop={true}
-      />
-    </main>
+    <>
+      <NavMenu />
+      <main>
+        <CosmicParallaxBg
+          head="Stickerific"
+          text="Your Vision, Your Sticker, Instantly"
+          loop={true}
+        />
+      </main>
+    </>
   );
 }
